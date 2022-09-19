b793555d6efae434f324bef5a80716139c5c508b
Jake Wiczyk <jwiczyk1@hwemail.com>
2022-09-18 17:14:49 -0700

    Tree fix
    Fixed tree

--- src/Tree.java
+++ src/Tree.java
@@ -13,32 +13,26 @@ public class Tree {
 		String str = "";
 		int count = 0;
 		for (String g : strList) {
-			if (count != 0) {
-				str+="\n";
-			}
 			count++;
 			str+= g;
+			if (count<strList.size()) {
+				str+="\n";
+			}
 		}
-		 // getInstance() method is called with algorithm SHA-1
+		
         MessageDigest md = MessageDigest.getInstance("SHA-1");
 
-        // digest() method is called
-        // to calculate message digest of the input string
-        // returned as array of byte
+      
         byte[] messageDigest = md.digest(str.getBytes());
 
-        // Convert byte array into signum representation
         BigInteger no = new BigInteger(1, messageDigest);
 
-        // Convert message digest into hex value
         String hashtext = no.toString(16);
 
-        // Add preceding 0s to make it 32 bit
         while (hashtext.length() < 32) {
             hashtext = "0" + hashtext;
         }
 
-        // return the HashText
         BufferedWriter writer = new BufferedWriter(new FileWriter("objects/"+hashtext));
         writer.write(str);
         writer.close();
